d2a0073776cea347bec43eb9169acc25b36c1e16
RosmyGeorge <rosmygeorge8@gmail.com>
2020-06-29 00:07:08 +0100

    Adding the steps file

---
+++ features/step-definitions/search_results_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am on holiday search results page$/)  do
+  visit SearchResultsPage
+end
+
+Then(/^I should see all the hotel names are displayed$/) do
+  (on SearchResultsPage).hotel_names
+end
+
+And(/^All hotel names should have prices displayed$/) do
+  (on SearchResultsPage).hotel_prices.should == true
+end
+
+When(/^I sort by "([^"]*)" value and should see the results sorted/) do |text|
+ (on SearchResultsPage).sorting(text)
+end
